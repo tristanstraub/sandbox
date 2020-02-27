e545c257801c6e2471a19e9558091b388c9aa69f
Tristan Straub <tristanstraub@gmail.com>
2020-02-27 16:11:48 +1100

    Make RUNTIME dynamic

--- clojure-postgres/start.sh
+++ clojure-postgres/start.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-password}
-RUNTIME=${RUNTIME:-podman}
 
-set -Euxo pipefail
+which podman;
 
-function db {
-    case $1 in
-        stop)
-            $RUNTIME stop postgres-sandbox
-            $RUNTIME rm postgres-sandbox
-            ;;
-        start)
-            POSTGRES_PASSWORD=$POSTGRES_PASSWORD $RUNTIME run --name postgres-sandbox -e POSTGRES_PASSWORD -p 5432:5432 -d postgres:11-alpine
-            ;;
-        create)
-            $RUNTIME exec -i postgres-sandbox psql -U postgres < db/create.sql
-            ;;
-        drop)
-            $RUNTIME exec -i postgres-sandbox psql -U postgres < db/drop.sql
-            ;;
-    esac
-}
+if [ $? -eq 0 ]; then
+    RUNTIME=${RUNTIME:-podman}
+else
+    RUNTIME=${RUNTIME:-docker}
+fi
 
-$@
+case $1 in
+    start)
+        POSTGRES_PASSWORD=$POSTGRES_PASSWORD $RUNTIME run --name postgres-sandbox -e POSTGRES_PASSWORD -p 5432:5432 -d postgres:11-alpine
+        ;;
+    init)
+        $RUNTIME exec -i postgres-sandbox psql -U postgres < db/create.sql
+        ;;
+    run)
+        (cd app; clj -m app.db DB_NAME sandbox DB_HOST localhost DB_USER app_user DB_PASSWORD $POSTGRES_PASSWORD)
+        ;;
+    drop)
+        $RUNTIME exec -i postgres-sandbox psql -U postgres < db/drop.sql
+        ;;
+    stop)
+        $RUNTIME stop postgres-sandbox
+        $RUNTIME rm postgres-sandbox
+        ;;
+esac
